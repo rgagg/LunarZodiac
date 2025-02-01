dc30264ff80d649cf586cabb6c60349fd142c8ce
Richard Gagg <r.gagg@outlook.com>
2025-02-01 15:21:41 +1100

    Initial Commit

---
+++ LunarZodiac/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LunarZodiac
+//
+//  Created by Richard Gagg on 1/2/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LunarZodiac/LunarZodiacApp.swift
@@ -0,0 +1,17 @@
+//
+//  LunarZodiacApp.swift
+//  LunarZodiac
+//
+//  Created by Richard Gagg on 1/2/2025.
+//
+
+import SwiftUI
+
+@main
+struct LunarZodiacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
