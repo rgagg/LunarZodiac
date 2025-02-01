37adea36d8366465e745cc62fee81e267f58ab5f
Richard Gagg <r.gagg@outlook.com>
2025-02-01 16:02:46 +1100

    Initail code base for Lunar Zodiac chellenge.

--- LunarZodiac/ContentView.swift
+++ LunarZodiac/ContentView.swift
@@ -8,17 +8,63 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+  
+  @State private var year: Int = 2025
+  @State private var imageNimber: Int = 5
+  
+  var body: some View {
+    VStack {
+      Text("\(year)")
+        .font(.largeTitle)
+        .fontWeight(.black)
+        .foregroundStyle(.red)
+      
+      Spacer()
+      
+      Image("image\(imageNimber)")
+        .resizable()
+        .scaledToFit()
+      
+      Spacer()
+      
+      HStack {
+        Button {
+          if year <= 2021 {
+            year = 2032
+            imageNimber = 11
+          } else {
+            year -= 1
+            imageNimber -= 1
+          }
+          year -= 1
+          imageNimber -= 1
+        } label: {
+          Image(systemName: "chevron.left.square.fill")
         }
-        .padding()
+        
+        Spacer()
+        
+        Button {
+          if year >= 2032 {
+            year = 2020
+            imageNimber = 0
+          } else {
+            year += 1
+            imageNimber += 1
+          }
+        } label: {
+          Image(systemName: "chevron.right.square.fill")
+        }
+      }
+      .font(.largeTitle)
+      .fontWeight(.black)
+      .foregroundStyle(.red)
+      .padding(.horizontal)
     }
+    .padding()
+  }
 }
 
 #Preview {
-    ContentView()
+  ContentView()
 }
